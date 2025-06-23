24ef2cae6ab07f4644f43598043cd08567629dd0
Yudith Zharifah <ifah77.utami@gmail.com>
2025-06-23 18:45:39 +0700

    revisi

--- Nomor 4/src/State.java
+++ Nomor 4/src/State.java
@@ -2,5 +2,5 @@
  *  The enum State contains the various game states of the TTT game
  */
 public enum State {  // to save as "State.java"
-    PLAYING, DRAW, CROSS_WON,NOUGHT_WON
+    PLAYING, DRAW, CROSS_WON, NOUGHT_WON
 }
